528f7c3921374c83afccdaeff80183e286d30aab
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-06 17:47:56 -0300

    salvando no localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import './styles.ts';
 import logoImg from '../../assets/githubExplorerLogo.svg';
 import { Title, Form, Repositories, Error } from './styles';
@@ -17,16 +17,27 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+    if (storedRepositories) {
+      return JSON.parse(storedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-    if (!inputError) {
-      setInputError('Digite o autor/nome do repositório');
-      return;
-    }
     if (!inputError) {
       setInputError('Digite o autor/nome do repositório');
       return;
